a55e0db09941cd40c770df53d4582ea87c2d96bb
Hex-Zero <hex0@live.com>
2020-12-11 12:46:23 +0000

    add title

--- assets/JavaScript/Layout.js
+++ assets/JavaScript/Layout.js
@@ -8,18 +8,22 @@ function loadPage(href) {
 document.getElementById("max-main").innerHTML = loadPage("../pages/index.html");
 
 $(".max-nav-link").click(function (e) {
+  $(".max-active-link").removeClass("max-active-link");
   $(".max-main").empty();
   document.getElementById("max-main").innerHTML = loadPage(
     e.currentTarget.dataset.href
   );
   $(".animated-icon1").click();
   if (e.currentTarget.dataset.hrefs) $.getScript(e.currentTarget.dataset.hrefs);
+  $(e.currentTarget).addClass("max-active-link");
 });
 
 $(".max-normal-link").click(function (e) {
+  $(".max-active-link").removeClass("max-active-link");
   $(".max-main").empty();
   document.getElementById("max-main").innerHTML = loadPage(
     e.currentTarget.dataset.href
   );
   if (e.currentTarget.dataset.hrefs) $.getScript(e.currentTarget.dataset.hrefs);
+  $(e.currentTarget).addClass("max-active-link");
 });
